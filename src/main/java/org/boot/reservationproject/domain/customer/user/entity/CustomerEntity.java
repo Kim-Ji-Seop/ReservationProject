fff388e23be94154b0b19a6304f94a2213779b29
kimjiseop <jskim2x@naver.com>
2024-06-26 21:38:22 +0900

    feat: Customer,SellerEntity 추가

---
+++ src/main/java/org/boot/reservationproject/domain/customer/user/entity/CustomerEntity.java
@@ -0,0 +1,45 @@
+package org.boot.reservationproject.domain.customer.user.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "customer")
+public class CustomerEntity {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id", nullable = false)
+  private Long id;
+
+  @Column(name = "email", nullable = false, length = 300)
+  private String email;
+
+  @Column(name = "password", nullable = false, length = 60)
+  private String password;
+
+  @Column(name = "phone_number", nullable = false, length = 11)
+  private String phoneNumber;
+
+  @Column(name = "birthday", nullable = false, length = 8)
+  private String birthday;
+
+  @Enumerated(EnumType.STRING)
+  @Column(name = "gender", nullable = false)
+  private Gender gender;
+
+  @Column(name = "name", length = 10)
+  private String name;
+
+  @Column(name = "nickname", nullable = false, length = 20, unique = true)
+  private String nickname;
+
+  public enum Gender {
+    MALE, FEMALE
+  }
+}

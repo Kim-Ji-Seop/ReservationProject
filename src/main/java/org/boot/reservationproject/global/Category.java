42e68f22ce2fe7baf4f2fb40128d0a9c6c4a37d8
kimjiseop <jskim2x@naver.com>
2024-07-11 19:34:09 +0900

    fix : 카테고리 분류 수정(모호한 분류 제거)

--- src/main/java/org/boot/reservationproject/global/Category.java
+++ src/main/java/org/boot/reservationproject/global/Category.java
@@ -8,12 +8,9 @@ import lombok.RequiredArgsConstructor;
 public enum Category {
   TOTAL("전체"),
   MOTEL("모텔"),
-  HOTEL("호텔"),
-  RESORT("리조트"),
-  PENSION("펜션"),
-  POOL_VILLA("풀빌라"),
-  CAMPING("캠핑"),
-  GLAMPING("글램핑");
+  HOTEL("호텔&리조트"),
+  PENSION("펜션&풀빌라"),
+  CAMPING("캠핑&글램핑");
 
   private final String categoryName;
 }

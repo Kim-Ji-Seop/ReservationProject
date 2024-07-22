87993a0c55e825a7e9f90d7b716bdd8e8422b209
Kimjiseop <jskim2x@naver.com>
2024-07-22 22:06:56 +0900

    fix: 배포판에 ElasticSearch 코드 제외

--- src/main/java/org/boot/reservationproject/global/elastic_search/ElasticSearchConfig.java
+++ src/main/java/org/boot/reservationproject/global/elastic_search/ElasticSearchConfig.java
@@ -29,60 +29,60 @@ import org.springframework.core.io.Resource;
 
 @Configuration
 public class ElasticSearchConfig {
-  @Value("${spring.elasticsearch.rest.username}")
-  private String username;
-
-  @Value("${spring.elasticsearch.rest.password}")
-  private String password;
-
-  @Value("${spring.elasticsearch.rest.uris}")
-  private String uri;
-
-
-  @Bean
-  public ElasticsearchClient elasticsearchClient() throws Exception {
-    // Basic 인증 정보 설정
-    final BasicCredentialsProvider credentialsProvider = new BasicCredentialsProvider();
-    credentialsProvider.setCredentials(AuthScope.ANY,
-        new UsernamePasswordCredentials(username, password));
-
-    RestClientBuilder builder = RestClient.builder(
-            HttpHost.create(uri))
-        .setHttpClientConfigCallback(httpClientBuilder -> httpClientBuilder
-            .setDefaultCredentialsProvider(credentialsProvider));
-
-    RestClient restClient = builder.build();
-
-    // ObjectMapper에 JavaTimeModule 등록
-    ObjectMapper objectMapper = new ObjectMapper();
-    objectMapper.registerModule(new JavaTimeModule());
-    JacksonJsonpMapper jsonpMapper = new JacksonJsonpMapper(objectMapper);
-
-    ElasticsearchClient client = new ElasticsearchClient(new RestClientTransport(restClient, jsonpMapper));
-
-    // 인덱스 생성
-    createIndexIfNotExists(client);
-
-    return client;
-  }
-  private void createIndexIfNotExists(ElasticsearchClient client) throws Exception {
-    // settings.json 파일 읽기
-    Resource resource = new ClassPathResource("settings.json");
-    InputStream is = resource.getInputStream();
-    String settingsJson = new String(is.readAllBytes());
-
-    // 인덱스 생성 요청
-    CreateIndexRequest createIndexRequest = new CreateIndexRequest.Builder()
-        .index("facilities")
-        .withJson(new ByteArrayInputStream(settingsJson.getBytes(StandardCharsets.UTF_8)))
-        .build();
-
-    // 인덱스가 없을 경우에만 생성
-    if (!client.indices().exists(b -> b.index("facilities")).value()) {
-      CreateIndexResponse createIndexResponse = client.indices().create(createIndexRequest);
-      if (!createIndexResponse.acknowledged()) {
-        throw new RuntimeException("Failed to create index 'facilities'");
-      }
-    }
-  }
+//  @Value("${spring.elasticsearch.rest.username}")
+//  private String username;
+//
+//  @Value("${spring.elasticsearch.rest.password}")
+//  private String password;
+//
+//  @Value("${spring.elasticsearch.rest.uris}")
+//  private String uri;
+//
+//
+//  @Bean
+//  public ElasticsearchClient elasticsearchClient() throws Exception {
+//    // Basic 인증 정보 설정
+//    final BasicCredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+//    credentialsProvider.setCredentials(AuthScope.ANY,
+//        new UsernamePasswordCredentials(username, password));
+//
+//    RestClientBuilder builder = RestClient.builder(
+//            HttpHost.create(uri))
+//        .setHttpClientConfigCallback(httpClientBuilder -> httpClientBuilder
+//            .setDefaultCredentialsProvider(credentialsProvider));
+//
+//    RestClient restClient = builder.build();
+//
+//    // ObjectMapper에 JavaTimeModule 등록
+//    ObjectMapper objectMapper = new ObjectMapper();
+//    objectMapper.registerModule(new JavaTimeModule());
+//    JacksonJsonpMapper jsonpMapper = new JacksonJsonpMapper(objectMapper);
+//
+//    ElasticsearchClient client = new ElasticsearchClient(new RestClientTransport(restClient, jsonpMapper));
+//
+//    // 인덱스 생성
+//    createIndexIfNotExists(client);
+//
+//    return client;
+//  }
+//  private void createIndexIfNotExists(ElasticsearchClient client) throws Exception {
+//    // settings.json 파일 읽기
+//    Resource resource = new ClassPathResource("settings.json");
+//    InputStream is = resource.getInputStream();
+//    String settingsJson = new String(is.readAllBytes());
+//
+//    // 인덱스 생성 요청
+//    CreateIndexRequest createIndexRequest = new CreateIndexRequest.Builder()
+//        .index("facilities")
+//        .withJson(new ByteArrayInputStream(settingsJson.getBytes(StandardCharsets.UTF_8)))
+//        .build();
+//
+//    // 인덱스가 없을 경우에만 생성
+//    if (!client.indices().exists(b -> b.index("facilities")).value()) {
+//      CreateIndexResponse createIndexResponse = client.indices().create(createIndexRequest);
+//      if (!createIndexResponse.acknowledged()) {
+//        throw new RuntimeException("Failed to create index 'facilities'");
+//      }
+//    }
+//  }
 }

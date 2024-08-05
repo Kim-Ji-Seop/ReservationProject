76b98d075adcfa37ae7e6235eac5db6012aa9cdc
Kimjiseop <jskim2x@naver.com>
2024-08-05 15:03:20 +0900

    fix : 배포판 제거

--- src/main/java/org/boot/reservationproject/domain/facility/FacilityController.java
+++ src/main/java/org/boot/reservationproject/domain/facility/FacilityController.java
@@ -37,102 +37,102 @@ import org.springframework.web.multipart.MultipartFile;
 @RequiredArgsConstructor
 @Slf4j
 public class FacilityController {
-//  private final FacilityService facilityService;
-//  @InitBinder
-//  public void initBinder(WebDataBinder binder) {
-//    binder.registerCustomEditor(Category.class, new CategoryConverter());
-//  }
-//  @PostMapping(value = "/registration", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-//  public ResponseEntity<BaseResponse<RegisterFacilityResponse>> registerFacility(
-//      @Valid @RequestPart("facilityRequest") RegisterFacilityRequest request, // json data
-//      @RequestPart("facilityPhotos") List<MultipartFile> facilityPhotos) throws IOException {
-//
-//    // 1. SecurityContextHolder에서 인증된 사용자 정보 추출
-//    String sellerEmail = SecurityContextHolder.getContext().getAuthentication().getName();
-//
-//    // 2. Service에 보내는 Param으로 추가
-//    RegisterFacilityResponse response =
-//        facilityService.registerFacility(request, facilityPhotos, sellerEmail);
-//    return ResponseEntity.ok(new BaseResponse<>(response));
-//  }
-//
-//  @PostMapping(value = "/registration/room-photos", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-//  public void registerRoomPhotos(
-//      @RequestParam("facilityIdx") Long facilityIdx,
-//      @RequestParam("roomIdx") Long roomIdx,
-//      @RequestPart("roomPhotos") List<MultipartFile> roomPhotos) throws IOException {
-//
-//    facilityService.registerRoomPhotos(facilityIdx, roomIdx, roomPhotos);
-//  }
-//
-//  // 시설들 카테고리별 조회
-//  @GetMapping( "/previews")
-//  public ResponseEntity<BaseResponse<FacilitiesPageResponse>>
-//              getFacilitiesPreview(
-//                  @RequestParam("category") Category category,
-//                  @RequestParam("checkIn") LocalDate checkInDate,
-//                  @RequestParam("checkOut") LocalDate checkOutDate,
-//                  @RequestParam("personal") int personal,
-//                  Pageable pageable){
-//    FacilitiesPageResponse responses =
-//        facilityService.getFacilitiesPreview(category, checkInDate, checkOutDate, personal, pageable);
-//    return ResponseEntity.ok(new BaseResponse<>(responses));
-//  }
-//
-//  // 시설 상세보기 조회
-//  @GetMapping( "/details/{facilityIdx}")
-//  public ResponseEntity<BaseResponse<FacilityInformationDetailResponse>>
-//              getFacilityDetail(
-//                  @PathVariable Long facilityIdx,
-//                  @RequestParam("checkIn") LocalDate checkInDate,
-//                  @RequestParam("checkOut") LocalDate checkOutDate,
-//                  @RequestParam("personal") int personal){
-//    FacilityInformationDetailResponse responses =
-//        facilityService.getFacilityDetail(facilityIdx, checkInDate, checkOutDate, personal);
-//    return ResponseEntity.ok(new BaseResponse<>(responses));
-//  }
-//
-//  // 시설정보 수정 - 기본 텍스트, 시설 사진, 객실
-//  @PatchMapping(value = "/{facilityIdx}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-//  public void updateFacility(
-//      @PathVariable Long facilityIdx,
-//      @Valid @RequestPart("facilityRequest") UpdateFacilityRequest request,
-//      @RequestPart("facilityPhotos") List<MultipartFile> facilityPhotos
-//  ) throws IOException {
-//
-//    facilityService.updateFacility(facilityIdx, request, facilityPhotos);
-//  }
-//
-//  // 객실 사진 수정
-//  @PatchMapping(value = "/room-photos", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-//  public void updateRoomPhotos(
-//      @RequestParam("facilityIdx") Long facilityIdx,
-//      @RequestParam("roomIdx") Long roomIdx,
-//      @RequestPart("roomPhotos") List<MultipartFile> roomPhotos) throws IOException {
-//
-//    facilityService.updateRoomPhotos(facilityIdx, roomIdx, roomPhotos);
-//  }
-//
-//  // 객실 추가(하나씩 추가) - ES 동기화
-//  @PostMapping(value = "/registration/additional-rooms", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-//  public void registerRooms(
-//      @RequestParam("facilityIdx") Long facilityIdx,
-//      @Valid @RequestPart("roomRequest") RegisterRoomRequest request,
-//      @RequestPart("roomPhotos") List<MultipartFile> roomPhotos) throws IOException {
-//
-//    facilityService.registerRooms(facilityIdx, request, roomPhotos);
-//  }
-//
-//  // 객실 삭제 - ES 동기화 (소프트 딜리트)
-//  @PatchMapping("/rooms/inactive")
-//  public void deleteRooms(
-//      @RequestParam("facilityIdx") Long facilityIdx,
-//      @RequestParam("roomIdx") Long roomIdx) throws IOException {
-//
-//    facilityService.deleteRooms(facilityIdx,roomIdx);
-//  }
-//
-//  // 시설에 포함된 모든 서비스 및 부대시설들 조회
-//
-//  // 시설, 객실들 사진 전체 조회 > 쿼리스트링
+  private final FacilityService facilityService;
+  @InitBinder
+  public void initBinder(WebDataBinder binder) {
+    binder.registerCustomEditor(Category.class, new CategoryConverter());
+  }
+  @PostMapping(value = "/registration", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+  public ResponseEntity<BaseResponse<RegisterFacilityResponse>> registerFacility(
+      @Valid @RequestPart("facilityRequest") RegisterFacilityRequest request, // json data
+      @RequestPart("facilityPhotos") List<MultipartFile> facilityPhotos) throws IOException {
+
+    // 1. SecurityContextHolder에서 인증된 사용자 정보 추출
+    String sellerEmail = SecurityContextHolder.getContext().getAuthentication().getName();
+
+    // 2. Service에 보내는 Param으로 추가
+    RegisterFacilityResponse response =
+        facilityService.registerFacility(request, facilityPhotos, sellerEmail);
+    return ResponseEntity.ok(new BaseResponse<>(response));
+  }
+
+  @PostMapping(value = "/registration/room-photos", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+  public void registerRoomPhotos(
+      @RequestParam("facilityIdx") Long facilityIdx,
+      @RequestParam("roomIdx") Long roomIdx,
+      @RequestPart("roomPhotos") List<MultipartFile> roomPhotos) throws IOException {
+
+    facilityService.registerRoomPhotos(facilityIdx, roomIdx, roomPhotos);
+  }
+
+  // 시설들 카테고리별 조회
+  @GetMapping( "/previews")
+  public ResponseEntity<BaseResponse<FacilitiesPageResponse>>
+              getFacilitiesPreview(
+                  @RequestParam("category") Category category,
+                  @RequestParam("checkIn") LocalDate checkInDate,
+                  @RequestParam("checkOut") LocalDate checkOutDate,
+                  @RequestParam("personal") int personal,
+                  Pageable pageable){
+    FacilitiesPageResponse responses =
+        facilityService.getFacilitiesPreview(category, checkInDate, checkOutDate, personal, pageable);
+    return ResponseEntity.ok(new BaseResponse<>(responses));
+  }
+
+  // 시설 상세보기 조회
+  @GetMapping( "/details/{facilityIdx}")
+  public ResponseEntity<BaseResponse<FacilityInformationDetailResponse>>
+              getFacilityDetail(
+                  @PathVariable Long facilityIdx,
+                  @RequestParam("checkIn") LocalDate checkInDate,
+                  @RequestParam("checkOut") LocalDate checkOutDate,
+                  @RequestParam("personal") int personal){
+    FacilityInformationDetailResponse responses =
+        facilityService.getFacilityDetail(facilityIdx, checkInDate, checkOutDate, personal);
+    return ResponseEntity.ok(new BaseResponse<>(responses));
+  }
+
+  // 시설정보 수정 - 기본 텍스트, 시설 사진, 객실
+  @PatchMapping(value = "/{facilityIdx}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+  public void updateFacility(
+      @PathVariable Long facilityIdx,
+      @Valid @RequestPart("facilityRequest") UpdateFacilityRequest request,
+      @RequestPart("facilityPhotos") List<MultipartFile> facilityPhotos
+  ) throws IOException {
+
+    facilityService.updateFacility(facilityIdx, request, facilityPhotos);
+  }
+
+  // 객실 사진 수정
+  @PatchMapping(value = "/room-photos", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+  public void updateRoomPhotos(
+      @RequestParam("facilityIdx") Long facilityIdx,
+      @RequestParam("roomIdx") Long roomIdx,
+      @RequestPart("roomPhotos") List<MultipartFile> roomPhotos) throws IOException {
+
+    facilityService.updateRoomPhotos(facilityIdx, roomIdx, roomPhotos);
+  }
+
+  // 객실 추가(하나씩 추가) - ES 동기화
+  @PostMapping(value = "/registration/additional-rooms", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+  public void registerRooms(
+      @RequestParam("facilityIdx") Long facilityIdx,
+      @Valid @RequestPart("roomRequest") RegisterRoomRequest request,
+      @RequestPart("roomPhotos") List<MultipartFile> roomPhotos) throws IOException {
+
+    facilityService.registerRooms(facilityIdx, request, roomPhotos);
+  }
+
+  // 객실 삭제 - ES 동기화 (소프트 딜리트)
+  @PatchMapping("/rooms/inactive")
+  public void deleteRooms(
+      @RequestParam("facilityIdx") Long facilityIdx,
+      @RequestParam("roomIdx") Long roomIdx) throws IOException {
+
+    facilityService.deleteRooms(facilityIdx,roomIdx);
+  }
+
+  // 시설에 포함된 모든 서비스 및 부대시설들 조회
+
+  // 시설, 객실들 사진 전체 조회 > 쿼리스트링
 }
--- src/main/java/org/boot/reservationproject/domain/search/service/FacilitySearchService.java
+++ src/main/java/org/boot/reservationproject/domain/search/service/FacilitySearchService.java
@@ -31,186 +31,186 @@ import org.springframework.stereotype.Service;
 @Slf4j
 @RequiredArgsConstructor
 public class FacilitySearchService {
-//  private final FacilityRepository facilityRepository;
-//  private final ElasticsearchClient elasticsearchClient;
-//  private final ElasticsearchOperations elasticsearchOperations;
-//
-//  // (임시) 테스트 끝날 때까진 이걸로 마이그레이션 계속.
-//  public void migrateFacilities() {
-//    List<Facility> facilities = facilityRepository.findAll();
-//    facilities.forEach(this::saveToElasticsearch);
-//  }
-//
-//  public void saveToElasticsearch(Facility facility) {
-//    List<RoomDocument> roomDocuments = facility.getRooms().stream()
-//        .map(room -> RoomDocument.builder()
-//            .roomIdx(room.getId())
-//            .roomName(room.getRoomName())
-//            .checkInTime(room.getCheckInTime())
-//            .checkOutTime(room.getCheckOutTime())
-//            .minPeople(room.getMinPeople())
-//            .maxPeople(room.getMaxPeople())
-//            .price(room.getPrice())
-//            .status(room.getStatus())
-//            .checkList(new ArrayList<>()) // 초기에는 빈 리스트로 설정
-//            .build())
-//        .toList();
-//
-//    FacilityDocument facilityDocument = FacilityDocument.builder()
-//        .id(facility.getId())
-//        .facilityName(facility.getFacilityName())
-//        .category(facility.getCategory())
-//        .region(facility.getRegion())
-//        .location(facility.getLocation())
-//        .averageRating(facility.getAverageRating())
-//        .numberOfReviews(facility.getNumberOfReviews())
-//        .previewFacilityPhotoUrl(facility.getPreviewFacilityPhotoUrl())
-//        .previewFacilityPhotoName(facility.getPreviewFacilityPhotoName())
-//        .facilityName_ngram(facility.getFacilityName())
-//        .region_ngram(facility.getRegion())
-//        .location_ngram(facility.getLocation())
-//        .rooms(roomDocuments)
-//        .build();
-//
-//    Optional<IndexResponse> response = saveDocumentToElasticsearch(facilityDocument);
-//
-//    response.ifPresentOrElse(
-//        r -> {
-//          log.info("Indexed with version {}", r.version());
-//          try {
-//            elasticsearchClient.indices().refresh(refreshRequest -> refreshRequest.index("facilities"));
-//          } catch (IOException e) {
-//            log.error("Failed to refresh index: {}", e.getMessage());
-//          }
-//        },
-//        () -> log.error("Failed to index facility: {}", facility.getId())
-//    );
-//  }
-//
-//  private Optional<IndexResponse> saveDocumentToElasticsearch(FacilityDocument facilityDocument) {
-//    try {
-//      IndexResponse response = elasticsearchClient.index(i -> i
-//          .index("facilities")
-//          .id(String.valueOf(facilityDocument.getId()))
-//          .document(facilityDocument)
-//      );
-//      return Optional.of(response);
-//    } catch (Exception e) {
-//      log.error("Error indexing document: {}", e.getMessage());
-//      return Optional.empty();
-//    }
-//  }
-//  public List<SearchKeywordResponse> searchByKeyword(String keyword, LocalDate checkInDate, LocalDate checkOutDate, int personal) throws IOException {
-//    SearchRequest searchRequest = new SearchRequest.Builder()
-//        .index("facilities")
-//        .query(q -> q
-//            .multiMatch(m -> m
-//                .fields("facilityName", "region", "location", "facilityName_ngram", "region_ngram", "location_ngram")
-//                .query(keyword)
-//            )
-//        ).build();
-//
-//    SearchResponse<FacilityDocument> searchResponse = elasticsearchClient.search(searchRequest, FacilityDocument.class);
-//    log.info("검색 쿼리 : {}", searchRequest);
-//    log.info("검색 결과 : {}", searchResponse);
-//    return searchResponse.hits().hits().stream()
-//        .map(Hit::source).filter(Objects::nonNull)
-//        .map(document -> filterRoomsByAvailability(document, checkInDate, checkOutDate, personal))
-//        .filter(Objects::nonNull)
-//        .map(this::convertToDto)
-//        .collect(Collectors.toList());
-//  }
-//
-//  private FacilityDocument filterRoomsByAvailability(FacilityDocument document, LocalDate checkInDate, LocalDate checkOutDate, int personal) {
-//    List<RoomDocument> availableRooms = document.getRooms().stream()
-//        .filter(room -> room.getStatus() != Status.DELETE)
-//        .filter(room -> (room.getMinPeople() <= personal) && (room.getMaxPeople() >= personal))
-//        .filter(room -> room.getCheckList().stream().noneMatch(checkList ->
-//            (checkInDate.isBefore(checkList.getCheckOutDate()) && checkOutDate.isAfter(checkList.getCheckInDate())) &&
-//                (checkList.getIsPaid() == Status.PAYMENT_FINISH || checkList.getIsPaid() == Status.PAYMENT_WAIT)
-//        ))
-//        .collect(Collectors.toList());
-//
-//    return FacilityDocument.builder()
-//        .id(document.getId())
-//        .facilityName(document.getFacilityName())
-//        .category(document.getCategory())
-//        .region(document.getRegion())
-//        .location(document.getLocation())
-//        .averageRating(document.getAverageRating())
-//        .numberOfReviews(document.getNumberOfReviews())
-//        .previewFacilityPhotoUrl(document.getPreviewFacilityPhotoUrl())
-//        .previewFacilityPhotoName(document.getPreviewFacilityPhotoName())
-//        .facilityName_ngram(document.getFacilityName())
-//        .region_ngram(document.getRegion())
-//        .location_ngram(document.getLocation())
-//        .rooms(availableRooms)
-//        .build();
-//  }
-//
-//  private SearchKeywordResponse convertToDto(FacilityDocument document) {
-//    List<RoomDocsPerFacility> roomDocs = document.getRooms().stream()
-//        .map(room -> RoomDocsPerFacility.builder()
-//            .roomIdx(room.getRoomIdx())
-//            .roomName(room.getRoomName())
-//            .checkInTime(room.getCheckInTime())
-//            .checkOutTime(room.getCheckOutTime())
-//            .minPeople(room.getMinPeople())
-//            .maxPeople(room.getMaxPeople())
-//            .price(room.getPrice())
-//            .status(room.getStatus())
-//            .checkListDocDtoList(room.getCheckList().stream()
-//                .map(checkList -> CheckListDocDto.builder()
-//                    .checkInDate(checkList.getCheckInDate())
-//                    .checkOutDate(checkList.getCheckOutDate())
-//                    .status(checkList.getIsPaid())
-//                    .build())
-//                .collect(Collectors.toList()))
-//            .build())
-//        .toList();
-//
-//    int minPrice = roomDocs.stream()
-//        .mapToInt(RoomDocsPerFacility::price)
-//        .min()
-//        .orElse(0);
-//
-//    return SearchKeywordResponse.builder()
-//        .id(document.getId())
-//        .facilityName(document.getFacilityName())
-//        .category(document.getCategory())
-//        .region(document.getRegion())
-//        .location(document.getLocation())
-//        .averageRating(document.getAverageRating())
-//        .numberOfReviews(document.getNumberOfReviews())
-//        .previewFacilityPhotoUrl(document.getPreviewFacilityPhotoUrl())
-//        .previewFacilityPhotoName(document.getPreviewFacilityPhotoName())
-//        .rooms(roomDocs)
-//        .minPrice(minPrice) // 0으로 표시되면 모든 객실 Soldou†
-//        .build();
-//  }
-//
-//  public List<String> autocompleteSearch(String keyword) throws IOException {
-//    SearchRequest searchRequest = new SearchRequest.Builder()
-//        .index("facilities")
-//        .query(q -> q
-//            .multiMatch(m -> m
-//                .fields("facilityName_ngram", "region_ngram", "location_ngram")
-//                .query(keyword)
-//            )
-//        ).build();
-//    log.info("자동완성 쿼리 : {}", searchRequest);
-//    SearchResponse<FacilityDocument> searchResponse = elasticsearchClient.search(searchRequest, FacilityDocument.class);
-//    log.info("자동완성 결과 : {}", searchResponse);
-//
-//    return searchResponse.hits().hits().stream()
-//        .flatMap(hit -> extractKeywords(hit.source(), keyword).stream())
-//        .distinct()
-//        .collect(Collectors.toList());
-//  }
-//
-//  private List<String> extractKeywords(FacilityDocument document, String keyword) {
-//    return Stream.of(document.getFacilityName(), document.getRegion(), document.getLocation())
-//        .filter(value -> value != null && value.contains(keyword))
-//        .collect(Collectors.toList());
-//  }
+  private final FacilityRepository facilityRepository;
+  private final ElasticsearchClient elasticsearchClient;
+  private final ElasticsearchOperations elasticsearchOperations;
+
+  // (임시) 테스트 끝날 때까진 이걸로 마이그레이션 계속.
+  public void migrateFacilities() {
+    List<Facility> facilities = facilityRepository.findAll();
+    facilities.forEach(this::saveToElasticsearch);
+  }
+
+  public void saveToElasticsearch(Facility facility) {
+    List<RoomDocument> roomDocuments = facility.getRooms().stream()
+        .map(room -> RoomDocument.builder()
+            .roomIdx(room.getId())
+            .roomName(room.getRoomName())
+            .checkInTime(room.getCheckInTime())
+            .checkOutTime(room.getCheckOutTime())
+            .minPeople(room.getMinPeople())
+            .maxPeople(room.getMaxPeople())
+            .price(room.getPrice())
+            .status(room.getStatus())
+            .checkList(new ArrayList<>()) // 초기에는 빈 리스트로 설정
+            .build())
+        .toList();
+
+    FacilityDocument facilityDocument = FacilityDocument.builder()
+        .id(facility.getId())
+        .facilityName(facility.getFacilityName())
+        .category(facility.getCategory())
+        .region(facility.getRegion())
+        .location(facility.getLocation())
+        .averageRating(facility.getAverageRating())
+        .numberOfReviews(facility.getNumberOfReviews())
+        .previewFacilityPhotoUrl(facility.getPreviewFacilityPhotoUrl())
+        .previewFacilityPhotoName(facility.getPreviewFacilityPhotoName())
+        .facilityName_ngram(facility.getFacilityName())
+        .region_ngram(facility.getRegion())
+        .location_ngram(facility.getLocation())
+        .rooms(roomDocuments)
+        .build();
+
+    Optional<IndexResponse> response = saveDocumentToElasticsearch(facilityDocument);
+
+    response.ifPresentOrElse(
+        r -> {
+          log.info("Indexed with version {}", r.version());
+          try {
+            elasticsearchClient.indices().refresh(refreshRequest -> refreshRequest.index("facilities"));
+          } catch (IOException e) {
+            log.error("Failed to refresh index: {}", e.getMessage());
+          }
+        },
+        () -> log.error("Failed to index facility: {}", facility.getId())
+    );
+  }
+
+  private Optional<IndexResponse> saveDocumentToElasticsearch(FacilityDocument facilityDocument) {
+    try {
+      IndexResponse response = elasticsearchClient.index(i -> i
+          .index("facilities")
+          .id(String.valueOf(facilityDocument.getId()))
+          .document(facilityDocument)
+      );
+      return Optional.of(response);
+    } catch (Exception e) {
+      log.error("Error indexing document: {}", e.getMessage());
+      return Optional.empty();
+    }
+  }
+  public List<SearchKeywordResponse> searchByKeyword(String keyword, LocalDate checkInDate, LocalDate checkOutDate, int personal) throws IOException {
+    SearchRequest searchRequest = new SearchRequest.Builder()
+        .index("facilities")
+        .query(q -> q
+            .multiMatch(m -> m
+                .fields("facilityName", "region", "location", "facilityName_ngram", "region_ngram", "location_ngram")
+                .query(keyword)
+            )
+        ).build();
+
+    SearchResponse<FacilityDocument> searchResponse = elasticsearchClient.search(searchRequest, FacilityDocument.class);
+    log.info("검색 쿼리 : {}", searchRequest);
+    log.info("검색 결과 : {}", searchResponse);
+    return searchResponse.hits().hits().stream()
+        .map(Hit::source).filter(Objects::nonNull)
+        .map(document -> filterRoomsByAvailability(document, checkInDate, checkOutDate, personal))
+        .filter(Objects::nonNull)
+        .map(this::convertToDto)
+        .collect(Collectors.toList());
+  }
+
+  private FacilityDocument filterRoomsByAvailability(FacilityDocument document, LocalDate checkInDate, LocalDate checkOutDate, int personal) {
+    List<RoomDocument> availableRooms = document.getRooms().stream()
+        .filter(room -> room.getStatus() != Status.DELETE)
+        .filter(room -> (room.getMinPeople() <= personal) && (room.getMaxPeople() >= personal))
+        .filter(room -> room.getCheckList().stream().noneMatch(checkList ->
+            (checkInDate.isBefore(checkList.getCheckOutDate()) && checkOutDate.isAfter(checkList.getCheckInDate())) &&
+                (checkList.getIsPaid() == Status.PAYMENT_FINISH || checkList.getIsPaid() == Status.PAYMENT_WAIT)
+        ))
+        .collect(Collectors.toList());
+
+    return FacilityDocument.builder()
+        .id(document.getId())
+        .facilityName(document.getFacilityName())
+        .category(document.getCategory())
+        .region(document.getRegion())
+        .location(document.getLocation())
+        .averageRating(document.getAverageRating())
+        .numberOfReviews(document.getNumberOfReviews())
+        .previewFacilityPhotoUrl(document.getPreviewFacilityPhotoUrl())
+        .previewFacilityPhotoName(document.getPreviewFacilityPhotoName())
+        .facilityName_ngram(document.getFacilityName())
+        .region_ngram(document.getRegion())
+        .location_ngram(document.getLocation())
+        .rooms(availableRooms)
+        .build();
+  }
+
+  private SearchKeywordResponse convertToDto(FacilityDocument document) {
+    List<RoomDocsPerFacility> roomDocs = document.getRooms().stream()
+        .map(room -> RoomDocsPerFacility.builder()
+            .roomIdx(room.getRoomIdx())
+            .roomName(room.getRoomName())
+            .checkInTime(room.getCheckInTime())
+            .checkOutTime(room.getCheckOutTime())
+            .minPeople(room.getMinPeople())
+            .maxPeople(room.getMaxPeople())
+            .price(room.getPrice())
+            .status(room.getStatus())
+            .checkListDocDtoList(room.getCheckList().stream()
+                .map(checkList -> CheckListDocDto.builder()
+                    .checkInDate(checkList.getCheckInDate())
+                    .checkOutDate(checkList.getCheckOutDate())
+                    .status(checkList.getIsPaid())
+                    .build())
+                .collect(Collectors.toList()))
+            .build())
+        .toList();
+
+    int minPrice = roomDocs.stream()
+        .mapToInt(RoomDocsPerFacility::price)
+        .min()
+        .orElse(0);
+
+    return SearchKeywordResponse.builder()
+        .id(document.getId())
+        .facilityName(document.getFacilityName())
+        .category(document.getCategory())
+        .region(document.getRegion())
+        .location(document.getLocation())
+        .averageRating(document.getAverageRating())
+        .numberOfReviews(document.getNumberOfReviews())
+        .previewFacilityPhotoUrl(document.getPreviewFacilityPhotoUrl())
+        .previewFacilityPhotoName(document.getPreviewFacilityPhotoName())
+        .rooms(roomDocs)
+        .minPrice(minPrice) // 0으로 표시되면 모든 객실 Soldou†
+        .build();
+  }
+
+  public List<String> autocompleteSearch(String keyword) throws IOException {
+    SearchRequest searchRequest = new SearchRequest.Builder()
+        .index("facilities")
+        .query(q -> q
+            .multiMatch(m -> m
+                .fields("facilityName_ngram", "region_ngram", "location_ngram")
+                .query(keyword)
+            )
+        ).build();
+    log.info("자동완성 쿼리 : {}", searchRequest);
+    SearchResponse<FacilityDocument> searchResponse = elasticsearchClient.search(searchRequest, FacilityDocument.class);
+    log.info("자동완성 결과 : {}", searchResponse);
+
+    return searchResponse.hits().hits().stream()
+        .flatMap(hit -> extractKeywords(hit.source(), keyword).stream())
+        .distinct()
+        .collect(Collectors.toList());
+  }
+
+  private List<String> extractKeywords(FacilityDocument document, String keyword) {
+    return Stream.of(document.getFacilityName(), document.getRegion(), document.getLocation())
+        .filter(value -> value != null && value.contains(keyword))
+        .collect(Collectors.toList());
+  }
 }
